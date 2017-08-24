699da5cbbd2792523f694f0bc8eca5ab2af717c6
neilmd <neil_capistrano2007@yahoo.com>
2017-08-24 18:45:31 +0800

    Added Nature of Activity

--- app/routes/APS_routes.js
+++ app/routes/APS_routes.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function (app, controllers){
-	router.get('/createGOSM', controllers.APS_controller.viewCreateGOSM);
-	router.post('/createGOSM', controllers.APS_controller.inputCreateGOSM);
+	// router.get('/createGOSM', controllers.APS_controller.viewCreateGOSM);
+	// router.post('/createGOSM', controllers.APS_controller.inputCreateGOSM);
 	return router;
 };
 
