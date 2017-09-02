304ee0abff89504516247cc2de430502e1eab55a
markusflores44 <markusflores44@gmail.com>
2017-09-02 16:25:48 +0800

    input activity requirements controller

--- app/controllers/APS_controller.js
+++ app/controllers/APS_controller.js
@@ -30,6 +30,67 @@ module.exports = function(database, models, queryFiles){
 				.catch(error => {
 
 				});
+		},
+
+		inputActivityRequirements: (req, res) => {
+
+			console.log(req.body);
+
+			//step 1
+			var perspective = req.body.perspective;
+			
+			//step 2
+			var date = [];
+			date = req.body['date[]'];
+			var timeStart = [];
+			timeStart = req.body['timeStart[]'];
+			var timeEnd = [];
+			timeEnd = req.body['timeEnd[]'];
+			var activity = [];
+			activity = req.body['activity[]'];
+			var description = [];
+			description = req.body['description[]'];
+			var personInCharge = [];
+			personInCharge = req.body['personInCharge'];
+
+			//step 3
+			var expenses = req.body.expenses;
+			var organizationalFunds = req.body.organizationalFunds;
+			var participantsFee = req.body.participantsFee;
+			var others = req.body.others;
+
+			var material = [];
+			material = req.body['material[]'];
+			var materialQuantity = [];
+			materialQuantity = req.body['materialQuantity[]'];
+			var unitCost = [];
+			unitCost = req.body['unitCost[]'];
+
+			//step 4
+			var operationalFund = req.body.operationalFund;
+			var depositoryFund = req.body.depositoryFund;
+			var otherSourceOfFunds = req.body.otherSourceOfFunds;
+
+			var revenueItem = [];
+			revenueItem = req.body['revenueItem[]'];
+			var revenueQuantity = [];
+			revenueQuantity = req.body['revenueQuantity[]'];
+			var revenueSellingPrice = [];
+			revenueSellingPrice = req.body['revenueSellingPrice[]'];
+
+
+			var expenseItem = [];
+			expenseItem = req.body['expenseItem[]'];
+			var expenseQuantity = [];
+			expenseQuantity = req.body['expenseQuantity[]'];
+			var expenseSellingPrice = [];
+			expenseSellingPrice = req.body['expenseSellingPrice[]'];
+
+			//step 5
+			var attachment = req.body.attachment;
+
+			var enp = req.body.enp;
+			var venue = req.body.venue;
 		}
 	};
 };
--- app/routes/APS_routes.js
+++ app/routes/APS_routes.js
@@ -5,6 +5,7 @@ module.exports = function (app, controllers){
 	router.get('/createGOSM', controllers.APS_controller.viewCreateGOSM);
 	router.get('/activityRequirements', controllers.APS_controller.createActivityRequirements);
 	router.post('/createGOSM', controllers.APS_controller.inputCreateGOSM);
+	router.post('/activityRequirements', controllers.APS_controller.inputActivityRequirements);
 	return router;
 };
 
