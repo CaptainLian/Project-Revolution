1e3da1127ee559be607f562b9bd39c5e2787d659
neilmd <neil_capistrano2007@yahoo.com>
2017-11-04 07:03:10 +0800

    Add View for Finance Settings

--- app/routes/Finance_routes.js
+++ app/routes/Finance_routes.js
@@ -2,5 +2,6 @@
 
 module.exports = function(configuration, modules, router, controllers, models, database, queryFiles){
 	router.get('/financeD', controllers.Finance_controller.viewDashboard);
+	router.get('/Finance/Settings', controllers.Finance_controller.viewFinanceSettings);
 	router.get('/transaction', controllers.Finance_controller.createTransaction);
 };
