878dc3ab4b46cec9c54967ab673248931052486e
neilmd <neil_capistrano2007@yahoo.com>
2017-11-04 09:05:36 +0800

    Added error checking Orgres Time Settings

--- app/controllers/ORGRES_controller.js
+++ app/controllers/ORGRES_controller.js
@@ -13,7 +13,26 @@ module.exports = function(configuration, modules, models, database, queryFiles){
             renderData.extra_data = req.extra_data;
 			return res.render('Orgres/ManageOrg', renderData);
 			//next();
+		},
+		viewManageTime: (req, res) => {
+			const renderData = Object.create(null);
+            renderData.extra_data = req.extra_data;
+			return res.render('Orgres/ManageTime', renderData);
+			//next();
+		},
+		submitTime: (req, res) => {
+			const renderData = Object.create(null);
+            renderData.extra_data = req.extra_data;
+
+            console.log(req.body);
+
+
+
+			return res.render('Orgres/ManageTime', renderData);
+			//next();
 		}
+
+
 
 
 	};
--- app/routes/Orgres_routes.js
+++ app/routes/Orgres_routes.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = function (configuration, modules, router, controllers, models, database, queryFiles){
-	router.get('/ORGRES/ManageAccount',controllers.ORGRES_controller.viewManageAccount);
-	router.get('/ORGRES/ManageOrganization',controllers.ORGRES_controller.viewManageOrg);
+	router.get('/ORGRES/Manage/Account',controllers.ORGRES_controller.viewManageAccount);
+	router.get('/ORGRES/Manage/Time',controllers.ORGRES_controller.viewManageTime);
+	router.get('/ORGRES/Manage/Organization',controllers.ORGRES_controller.viewManageOrg);
 	
+	router.post('/orgres/time/submit',controllers.ORGRES_controller.submitTime);
 
 };
