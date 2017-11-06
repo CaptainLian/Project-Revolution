b01457485714365a1759803893ef8d445c532ac3
neilmd <neil_capistrano2007@yahoo.com>
2017-11-06 17:58:34 +0800

    Start AMT assign checker

--- app/models/ActivityMonitoring_model.js
+++ app/models/ActivityMonitoring_model.js
@@ -4,11 +4,16 @@ module.exports = function(configuration, modules, database, queryFiles){
 	const ActivityMonitoringModel = Object.create(null);
 
 	const insertAMTActivityEvaluationSQL = queryFiles.insertAMTActivityEvaluation;
+	const getAvailableActivityToCheck = queryFiles.getAvailableActivityToCheck;
 	
 	ActivityMonitoringModel.insertAMTActivityEvaluation = function(param, connection = database) {
         
         return connection.none(insertAMTActivityEvaluationSQL, param);
     };
+    ActivityMonitoringModel.getAvailableActivityToCheck = function(param, connection = database){
+    	let param = Object.create(null);
+    	return connection.any(getAvailableActivityToCheck, param);
+    }
 
 
 	return ActivityMonitoringModel;
