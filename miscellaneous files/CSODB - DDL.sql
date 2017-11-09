d2b3bc975d626347bcaceb66c09ae03c4be1f655
Lian <juliano_laguio@dlsu.edu.ph>
2017-11-09 20:09:22 +0800

    Added database tables

--- miscellaneous files/CSODB - DDL.sql
+++ miscellaneous files/CSODB - DDL.sql
@@ -884,21 +884,21 @@ $trigger$
                                              (executiveSecretariatRoleID, (SELECT id FROM functionality WHERE(id%1000 = 10)), TRUE);
 
         INSERT INTO OrganizationRole(organization, name, uniquePosition, masterRole, rank)
-                             VALUES (NEW.id, 'External Executive Vice President', TRUE, presidentRoleID, 20)
+                             VALUES (NEW.id, 'External Executive Vice President', TRUE, presidentRoleID, 10)
         RETURNING id INTO eevpRoleID;
         INSERT INTO OrganizationAccessControl (role, functionality, isAllowed)
                                       VALUES  (eevpRoleID, (SELECT id FROM functionality WHERE(id%1000 = 9)), TRUE),
                                               (eevpRoleID, (SELECT id FROM functionality WHERE(id%1000 = 10)), TRUE);
 
         INSERT INTO OrganizationRole(organization, name, uniquePosition, masterRole, rank)
-                             VALUES (NEW.id, 'Internal Executive Vice President', TRUE, presidentRoleID, 30)
+                             VALUES (NEW.id, 'Internal Executive Vice President', TRUE, presidentRoleID, 10)
         RETURNING id INTO ievpRoleID;
         INSERT INTO OrganizationAccessControl (role, functionality, isAllowed)
                                       VALUES  (ievpRoleID, (SELECT id FROM functionality WHERE(id%1000 = 9)), TRUE),
                                               (ievpRoleID, (SELECT id FROM functionality WHERE(id%1000 = 10)), TRUE);
 
         INSERT INTO OrganizationRole(organization, name, uniquePosition, masterRole, rank)
-                             VALUES (NEW.id, 'Vice President of Documentations', TRUE, executiveSecretariatRoleID, 40)
+                             VALUES (NEW.id, 'Vice President of Documentations', TRUE, executiveSecretariatRoleID, 20)
         RETURNING id INTO vpdRoleID;
         INSERT INTO OrganizationAccessControl (role, functionality, isAllowed)
                                       VALUES  (vpdRoleID, (SELECT id FROM functionality WHERE(id%1000 = 9)), TRUE),
@@ -907,14 +907,14 @@ $trigger$
                                               (vpdRoleID, (SELECT id FROM functionality WHERE(id%1000 = 12)), TRUE);
 
         INSERT INTO OrganizationRole(organization, name, uniquePosition, masterRole, rank)
-                             VALUES (NEW.id, 'Associate Vice President of Documentations', FALSE, vpdRoleID, 50)
+                             VALUES (NEW.id, 'Associate Vice President of Documentations', FALSE, vpdRoleID, 30)
         RETURNING id INTO avpdRoleID;
         INSERT INTO OrganizationAccessControl (role, functionality, isAllowed)
                                       VALUES  (avpdRoleID, (SELECT id FROM functionality WHERE(id%1000 = 9)), TRUE),
                                               (avpdRoleID, (SELECT id FROM functionality WHERE(id%1000 = 10)), TRUE);
 
         INSERT INTO OrganizationRole(organization, name, uniquePosition, masterRole, home_url, rank)
-                             VALUES (NEW.id, 'Vice President of Finance', TRUE, ievpRoleID, '/Organization/treasurer/dashboard', 60)
+                             VALUES (NEW.id, 'Vice President of Finance', TRUE, ievpRoleID, '/Organization/treasurer/dashboard', 20)
         RETURNING id INTO vpfRoleID;
         INSERT INTO OrganizationAccessControl (role, functionality, isAllowed)
                                       VALUES  (vpfRoleID, (SELECT id FROM functionality WHERE(id%1000 = 9)), TRUE),
@@ -923,7 +923,7 @@ $trigger$
                                               (vpdRoleID, (SELECT id FROM functionality WHERE(id%1000 = 11)), TRUE);
 
         INSERT INTO OrganizationRole(organization, name, uniquePosition, masterRole, home_url, rank)
-                             VALUES (NEW.id, 'Associate Vice President of Finance', FALSE, vpfRoleID, '/Organization/treasurer/dashboard', 70)
+                             VALUES (NEW.id, 'Associate Vice President of Finance', FALSE, vpfRoleID, '/Organization/treasurer/dashboard', 30)
         RETURNING id INTO avpfRoleID;
         INSERT INTO OrganizationAccessControl (role, functionality, isAllowed)
                                       VALUES  (avpfRoleID, (SELECT id FROM functionality WHERE(id%1000 = 9)), TRUE),
@@ -1498,6 +1498,7 @@ $trigger$
          FROM GOSMActivityProjectHead
         WHERE activityID = NEW.GOSMActivity;
 
+
     /*
         -- ALL PROJECT HEADS
         INSERT INTO ProjectProposalSignatory (GOSMActivity, signatory, type)
@@ -1680,14 +1681,6 @@ CREATE TABLE "ActivityResearchForm" (
 
 /* ADM */
   /* Post Acts */
-  -- Opened 
-  -- In Progress
-  -- Submitted
-  -- 
-
--- BriefContext
--- Others Finance Doucments
--- Finance Document
 DROP TABLE IF EXISTS "PostProjectProposalStatus" CASCADE;
 CREATE TABLE "PostProjectProposalStatus" (
   id SMALLINT,
@@ -1696,13 +1689,15 @@ CREATE TABLE "PostProjectProposalStatus" (
   PRIMARY KEY(id)
 );
 INSERT INTO "PostProjectProposalStatus" (id, name)
-                                 VALUES ( 0, 'Opened'),
-                                        ( 1, 'In-progress'),
-                                        ( 2, 'Submitted');
+                                 VALUES ( 0, 'Unopened'),
+                                        ( 1, 'Opened'),
+                                        ( 2, 'In-progress'),
+                                        ( 3, 'Submitted');
 
 DROP TABLE IF EXISTS "PostProjectProposal" CASCADE;
 CREATE TABLE "PostProjectProposal" (
   "GOSMActivity" INTEGER REFERENCES ProjectProposal(GOSMActivity),
+  "preparedBy" INTEGER REFERENCES Account(idNumber),
   "status" SMALLINT NOT NULL DEFAULT 0 REFERENCES "PostProjectProposalStatus"("id"),
   "dateCreated" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "objectives" TEXT[],
@@ -1714,6 +1709,9 @@ CREATE TABLE "PostProjectProposal" (
   "isBriefContextCompleted" BOOLEAN NOT NULL DEFAULT FALSE,
   "isOtherFinanceDocumentsCompleted" BOOLEAN NOT NULL DEFAULT FALSE,
   "isFinanceDocumentCompleted" BOOLEAN NOT NULL DEFAULT FALSE,
+  "isCheckedDP" BOOLEAN NOT NULL DEFAULT FALSE,
+  "isCheckedR" BOOLEAN NOT NULL DEFAULT FALSE,
+  "isCheckedBT" BOOLEAN NOT NULL DEFAULT FALSE,
 
   PRIMARY KEY("GOSMActivity")
 );
@@ -1770,6 +1768,71 @@ CREATE TRIGGER "before_insert_PostProjectProposalEventPicture"
     BEFORE INSERT ON "PostProjectProposalEventPicture"
     FOR EACH ROW
     EXECUTE PROCEDURE "trigger_before_insert_PostProjectProposalEventPicture"();
+/*
+DROP TABLE IF EXISTS "" CASCADE;
+CREATE TABLE "" (
+
+);
+*/
+DROP TABLE IF EXISTS "PostProjectDirectPaymentPayment" CASCADE;
+CREATE TABLE "PostProjectDirectPaymentPayment" (
+  id INTEGER,
+  name VARCHAR(45),
+
+  PRIMARY KEY (id)
+);
+INSERT INTO "PostProjectDirectPaymentPayment" (id, name)
+                                 VALUES ( 0, 'Cheque'),
+                                        ( 1, 'Book Transfer');
+DROP TABLE IF EXISTS "PostProjectDirectPayment" CASCADE;
+CREATE TABLE "PostProjectDirectPayment" (
+  "GOSMActivity" INTEGER REFERENCES "PostProjectProposal"("GOSMActivity"),
+  "nameOfEstablishment" VARCHAR(60),
+  "amount" NUMERIC(12, 2),
+  "paymentBy" SMALLINT REFERENCES "PostProjectDirectPaymentPayment"("id"),
+  "delayedProcessing" TEXT,
+  "path_formalQuotation" TEXT,
+  "path_ROF" TEXT,
+
+  PRIMARY KEY("GOSMActivity")
+);
+
+DROP TABLE IF EXISTS "PostProjectReimbursementPayment" CASCADE;
+CREATE TABLE "PostProjectReimbursementPayment" (
+  id INTEGER,
+  name VARCHAR(45),
+
+  PRIMARY KEY (id)
+);
+INSERT INTO "PostProjectReimbursementPayment" (id, name)
+                                 VALUES ( 0, 'Payment By Check'),
+                                        ( 1, 'Payment By Book Transfer');
+DROP TABLE IF EXISTS "PostProjectReimbursement" CASCADE;
+CREATE TABLE "PostProjectReimbursement" (
+  "GOSMActivity" INTEGER REFERENCES "PostProjectProposal"("GOSMActivity"),
+  "nameOfEstablishment" VARCHAR(60),
+  "amount" NUMERIC(12, 2),
+  "paymentBy" SMALLINT REFERENCES "PostProjectReimbursementPayment"("id"),
+  "foodExpense" VARCHAR(60),
+  "NUCAODP" TEXT,
+  "delayedProcessing" TEXT,
+  "path_receipts" TEXT,
+
+  PRIMARY KEY("GOSMActivity")
+);
+
+DROP TABLE IF EXISTS "PostProjectBookTransfer" CASCADE;
+CREATE TABLE "PostProjectBookTransfer" (
+  "GOSMActivity" INTEGER REFERENCES "PostProjectProposal"("GOSMActivity"),
+  "nameOfEstablishment" VARCHAR(60),
+  "amount" NUMERIC(12, 2),
+  "purpose" VARCHAR(60),
+  "path_billingStatement" TEXT,
+  "path_requisitionSlip" TEXT,
+  "path_chargeSlip" TEXT,
+
+  PRIMARY KEY ("GOSMActivity")
+);
   /* Post Acts END*/
 /* ADM END */
 /*
