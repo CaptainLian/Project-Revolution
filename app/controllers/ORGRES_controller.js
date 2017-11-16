c3be78eca74c230d9182c8bbbd637c580163335f
markusflores44 <markusflores44@gmail.com>
2017-11-17 00:31:43 +0800

    dropdown in orgres research form done but still doesnt react

--- app/controllers/ORGRES_controller.js
+++ app/controllers/ORGRES_controller.js
@@ -1,5 +1,8 @@
 module.exports = function(configuration, modules, models, database, queryFiles){
 
+	const orgresModel = models.Orgres_model;
+
+
 	return {
 		viewManageAccount: (req, res) => {
 			const renderData = Object.create(null);
@@ -30,11 +33,42 @@ module.exports = function(configuration, modules, models, database, queryFiles){
 			return res.render('Orgres/ManageTime', renderData);
 		},
 
-		viewSubmitResarchActivityForm: (req, res) => {
+		viewSubmitResearchActivityForm: (req, res) => {
+
+
+			database.task(task => {
+                        return task.batch([
+							orgresModel.getActivitiesForResearchForm(),
+							orgresModel.getOrganizationsForResearchForm()
+                        ]);
+            }).then(data=>{
+            	const renderData = Object.create(null);
+	            renderData.extra_data = req.extra_data;
+	            renderData.csrfToken = req.csrfToken();
+	            renderData.activities = data[0];
+	            renderData.organizations = data[1];
+
+
+				return res.render('Orgres/SubmitActivityResearchForm', renderData);
+
+            }).catch(error=>{
+            	console.log(error);
+            });
+
+		},
+
+		submitResearchActivityForm: (req, res) =>{
+
+			console.log("enters this");
+			console.log(req.body);
+
 			const renderData = Object.create(null);
             renderData.extra_data = req.extra_data;
+            renderData.csrfToken = req.csrfToken();
 
-			return res.render('Orgres/SubmitActivityReearchForm', renderData);
+
+			return res.render('Orgres/SubmitActivityResearchForm', renderData);
+			
 		}
 
 	};
