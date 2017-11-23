eded7f4138c1708239d775855f23e4d4d9f1d047
Lian <juliano_laguio@dlsu.edu.ph>
2017-11-24 01:45:09 +0800

    ADDED KARL MARX QUOTES RANDOMLY SELECTED

---
+++ app/pre_middlewares/Quote_footer_middleware.js
@@ -0,0 +1,35 @@
+'use strict';
+//configuration, mainApplication, modules, database, queryFiles, models
+module.exports = function(configuration, application, modules, database, queryFiles, models) {
+    const Promise = modules.Promise;
+
+    const logger = modules.logger;
+    const log_options = {
+        from: 'Quote-footer-middleware'
+    };
+
+    const QUOTES = [
+        'The worker of the world has nothing to lose, but their chains, workers of the world unite.',
+        'Revolutions are the locomotives of history',
+        'A specter is haunting Europe - the specter of communism.',
+        'Religion is the opium of the masses.',
+        'Machines were, it may be said, the weapon employed by the capitalists to quell the revolt of specialized labor.',
+        'Capital is reckless of the health or length of life of the laborer, unless under compulsion from society.',
+        'The first requisite for the happiness of the people is the abolition of religion.'
+    ];
+
+    const FooterAttacher = Object.create(null);
+    FooterAttacher.name = 'Quote-footer-middleware';
+    FooterAttacher.priority = configuration.load_priority.LOW;
+    FooterAttacher.action = (req, res, next) => {
+        logger.debug(`Attaching footer data` ,log_options);
+
+        req.extra_data.footer = QUOTES[Math.floor(Math.random()*(QUOTES.length))];
+
+        logger.debug(`Chosen quote: ${req.extra_data.footer}`,log_options);
+
+        return next();
+    };
+
+    return [FooterAttacher];
+};
