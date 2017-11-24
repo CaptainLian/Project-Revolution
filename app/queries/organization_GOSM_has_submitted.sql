34714b424bb30320bcd7e235c8c7561dc9875acb
Lian <juliano_laguio@dlsu.edu.ph>
2017-11-24 11:32:36 +0800

    Fixed GOSM not appearing on pend

--- app/queries/organization_GOSM_has_submitted.sql
+++ app/queries/organization_GOSM_has_submitted.sql
@@ -2,4 +2,6 @@ SELECT EXISTS(SELECT *
                 FROM GOSM
                WHERE termID = system_get_current_term_id()
                  AND studentOrganization = ${studentOrganization}
-                 AND status <> 1) AS "exists";
+                 AND status <> 1
+                 AND status <> 4
+                 AND status <> 5) AS "exists";
