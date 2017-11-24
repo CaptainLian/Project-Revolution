a0b1885d7aa731e25b4104db5f8835e1cfd12ca3
markusflores44 <markusflores44@gmail.com>
2017-11-24 11:07:43 +0800

    activity to implement again

--- app/queries/getGOSMActivitiesToImplement.sql
+++ app/queries/getGOSMActivitiesToImplement.sql
@@ -3,8 +3,7 @@ SELECT id, strategies, to_char(G.targetdatestart, 'Mon DD, YYYY') AS startdate, 
   		  FROM gosmactivity
  		 WHERE id NOT IN(SELECT GOSMACTIVITY
 		 				   FROM PROJECTPROPOSAL))
- UNION (SELECT *, status as status
- 		  FROM gosmactivity
- 		 WHERE id IN(SELECT GOSMACTIVITY
-	      			   FROM PROJECTPROPOSAL))) G
+ UNION (SELECT GA.*, P.status as status
+ 		  FROM gosmactivity GA JOIN PROJECTPROPOSAL P
+ 		  						 ON GA.ID=P.GOSMACTIVITY)) G
  WHERE gosm=${gosm};
