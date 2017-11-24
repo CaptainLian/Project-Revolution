04774095b9d5597fbd443bc3572cc62aec7e17d4
markusflores44 <markusflores44@gmail.com>
2017-11-24 11:44:02 +0800

    project head only ppr again

--- app/queries/getGOSMActivitiesToImplement.sql
+++ app/queries/getGOSMActivitiesToImplement.sql
@@ -1,6 +1,6 @@
 SELECT id, strategies, to_char(G.targetdatestart, 'Mon DD, YYYY') AS startdate, status
   FROM ((SELECT GA.*, 0 as status
-  		  FROM gosmactivity GA JOIN GOSMACITVITYPROJECTHEAD GAPH
+  		  FROM gosmactivity GA JOIN GOSMACTIVITYPROJECTHEAD GAPH
   		  						 ON GAPH.ACTIVITYID=GA.ID
  		 WHERE GAPH.idnumber = ${idnumber}
  		   AND id NOT IN(SELECT GOSMACTIVITY
@@ -8,7 +8,7 @@ SELECT id, strategies, to_char(G.targetdatestart, 'Mon DD, YYYY') AS startdate, 
  UNION (SELECT GA.*, P.status as status
  		  FROM gosmactivity GA JOIN PROJECTPROPOSAL P
  		  						 ON GA.ID=P.GOSMACTIVITY
- 		  					   JOIN GOSMAC GAPH
+ 		  					   JOIN GOSMACTIVITYPROJECTHEAD GAPH
  		  					     ON GAPH.ACTIVITYID=GA.ID
  		 WHERE GAPH.idnumber = ${idnumber})) G
  WHERE gosm=${gosm};
