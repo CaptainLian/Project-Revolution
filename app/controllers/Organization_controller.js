7e56cc1fca9fb84ad8a8874c836e151d470099d5
neilmd <neil_capistrano2007@yahoo.com>
2018-01-21 07:44:10 +0800

    Fixes

--- app/controllers/Organization_controller.js
+++ app/controllers/Organization_controller.js
@@ -1179,9 +1179,12 @@ module.exports = function(configuration, modules, models, database, queryFiles) 
                         depositoryfunds: orgdata.depositryfunds
                     };
 
-
+                    console.log(dbParam)
                     projectProposalModel.insertProjectProposal(dbParam).then(data => {
+                        console.log("DATA  NIL")
+                        console.log(data)
                         database.task(task => {
+
                             return task.batch([
                                 gosmModel.getGOSMActivity(dbParam),
                                 gosmModel.getGOSMActivityProjectHeads(dbParam),
@@ -1280,13 +1283,20 @@ module.exports = function(configuration, modules, models, database, queryFiles) 
 
                             return res.render('Org/SubmitProjectProposal_main', renderData);
                         }).catch(err => {
-                            logger.error(`${err.message}\n${err.stack}`, log_options);
+
+                            console.log("err 3  ")
+                            console.log(err)
+                            // logger.error(`${err.message}\n${err.stack}`, log_options);
 
                         });
+                    }).catch(err=>{
+                         console.log("err 2 ")
+                            console.log(err)
                     })
 
                 }).catch(error => {
-
+                    console.log("err4")
+                     console.log(err)
                 });
 
 
