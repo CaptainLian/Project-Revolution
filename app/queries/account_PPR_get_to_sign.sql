d2aa6886c81ad080bf0e5569e922d8a8f131ef27
Lian <juliano_laguio@dlsu.edu.ph>
2018-02-20 15:30:24 +0800

    What

--- app/queries/account_PPR_get_to_sign.sql
+++ app/queries/account_PPR_get_to_sign.sql
@@ -10,30 +10,15 @@ WITH "CurrentTermPPR" AS (
                                                    AND g.status = 3))
   ORDER BY id ASC
 ),"AccountToSign" AS ( /* The minimum type of the user to sign PPRs */
-    SELECT pps.GOSMActivity, MIN(st.lineup) AS lineup
+    SELECT DISTINCT pps.GOSMActivity
       FROM ProjectProposalSignatory pps LEFT JOIN SignatoryType st
                                                ON pps.type = st.id
      WHERE pps.signatory = ${idNumber}
        AND pps.GOSMActivity IN (SELECT GOSMActivity FROM "CurrentTermPPR")
-       AND status = 0
-  GROUP BY pps.GOSMActivity
-), "ProjectProposalCurrentLineup" AS ( /* The actual minimum type of each PPR*/
-    SELECT pps.GOSMActivity, MIN(st.lineup) AS lineup
-      FROM ProjectProposalSignatory pps LEFT JOIN SignatoryType st
-                                               ON pps.type = st.id
-     WHERE GOSMActivity IN (SELECT GOSMActivity FROM "CurrentTermPPR")
-       AND status = 0
-  GROUP BY pps.GOSMActivity
-), "AccountPPRCanSign" AS (
-    -- This is the list of ProjectProposals the user can currently sign
-    SELECT ppcl.GOSMActivity
-      FROM "ProjectProposalCurrentLineup" ppcl INNER JOIN "AccountToSign" ats
-                                                       ON ppcl.GOSMActivity = ats.GOSMActivity
-                                                      AND ppcl.lineup = ats.lineup
 )
 SELECT pp.GOSMActivity, to_char(pp.actualDateStart, 'Mon DD, YYYY') AS actualDateStart, ga.strategies
   FROM (SELECT *
           FROM ProjectProposal
          WHERE GOSMActivity IN (SELECT GOSMActivity
-                                  FROM "AccountPPRCanSign")) pp LEFT JOIN GOSMActivity ga
+                                  FROM "AccountToSign")) pp LEFT JOIN GOSMActivity ga
                                                                        ON pp.GOSMActivity = ga.id;
