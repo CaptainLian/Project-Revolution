ddb9bf68bfa0a7b256f785416c3bfdb587c6b4c6
neilmd <neil_capistrano2007@yahoo.com>
2018-02-21 17:53:46 +0800

    Add get functionalities and get org roles + fix some bugs

--- app/assets/js/Organization/Settings_ACL.js
+++ app/assets/js/Organization/Settings_ACL.js
@@ -1,15 +1,18 @@
-var oTable = $('#myTable').DataTable(   {
-	scrollY:        "300px",
-	scrollX:        true,
-	scrollCollapse: true,
-	paging:         false,
-	fixedColumns:   {
-		leftColumns: 1,
-	},
-	"ordering": true,
-    columnDefs: [{
-      orderable: false,
-      targets: "no-sort"
-    }]
+$(document).ready(function(){
+	var oTable = $('#myTable').DataTable(   {
+				scrollY:        "300px",
+				scrollX:        true,
+				scrollCollapse: false,
+				paging:         true,
+				fixedColumns:   {
+					leftColumns: 1,
+				},
+				"ordering": false,
+			    columnDefs: [{
+			      orderable: false,
+			      
+			    }]
 
-} );
+			} ).draw();
+
+})
