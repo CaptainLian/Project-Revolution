4421fa28007a5dabe904d1d51ff2098a98a9e3a9
neilmd <neil_capistrano2007@yahoo.com>
2018-02-23 19:46:24 +0800

    Add status signatory list and remove buttons on view of that activity

--- app/queries/account_PPR_get_to_sign.sql
+++ app/queries/account_PPR_get_to_sign.sql
@@ -9,15 +9,14 @@ WITH "CurrentTermPPR" AS (
                                                    AND g.status = 3))
   ORDER BY id ASC
 ),"AccountToSign" AS ( /* The minimum type of the user to sign PPRs */
-    SELECT DISTINCT pps.GOSMActivity
+    SELECT DISTINCT pps.GOSMActivity AS GOSMActivity, status
       FROM ProjectProposalSignatory pps LEFT JOIN SignatoryType st
                                                ON pps.type = st.id
      WHERE pps.signatory = ${idNumber}
        AND pps.GOSMActivity IN (SELECT GOSMActivity FROM "CurrentTermPPR")
 )
-SELECT pp.GOSMActivity, to_char(pp.actualDateStart, 'Mon DD, YYYY') AS actualDateStart, ga.strategies
-  FROM (SELECT *
-          FROM ProjectProposal
-         WHERE GOSMActivity IN (SELECT GOSMActivity
-                                  FROM "AccountToSign")) pp LEFT JOIN GOSMActivity ga
+SELECT pp.GOSMActivity, to_char(pp.actualDateStart, 'Mon DD, YYYY') AS actualDateStart, ga.strategies, pp.status
+  FROM (SELECT pp.actualDateStart, pp.GOSMActivity, ats.status
+          FROM ProjectProposal PP RIGHT JOIN "AccountToSign" ATS
+                                          ON PP.GOSMActivity = ATS.GOSMActivity) pp LEFT JOIN GOSMActivity ga
                                                                        ON pp.GOSMActivity = ga.id;
