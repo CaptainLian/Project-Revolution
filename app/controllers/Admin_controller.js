22286a82da41eec9c256875a38c9f497813ed97a
neilmd <neil_capistrano2007@yahoo.com>
2018-03-07 17:23:03 +0800

    TEST backup

--- app/controllers/Admin_controller.js
+++ app/controllers/Admin_controller.js
@@ -1,10 +1,16 @@
 'use strict';
-const  PgTools= require('pg-tools');
+const  PgTools = require('pg-tools');
+const  configuration = require('../../config.js');
 
 module.exports = function(configuration, modules, models, database, queryFiles){
 	const AdminController = Object.create(null);
-	
+	const path =  require('path')
 	const accountModel = models.Account_model;
+	    var Class = require('ee-class')
+            , util = require('util')            
+            , exec = require('child_process').exec
+            , fs = require('fs')
+            , asyncMethod = require('async-method');
 	
 
 
@@ -22,32 +28,42 @@ module.exports = function(configuration, modules, models, database, queryFiles){
 
 	AdminController.import = (req, res) => {
 		var tool = new PgTools();
-		 tool.restoreDatabase({
-		    host: 'localhost'
-		    , port: 5432
-		    , user: 'postgres'	
-		    , password: 'postgres'	    
-		    ,dumpPath: '/home/shinichi/dir2/test.sql'
-		    , database: 'CSODB'
-		}, function (error, output, message) {
-		    console.log(error)
-		});
+		//  tool.restoreDatabase({
+		//     host: 'localhost'
+		//     , port: 5432
+		//     , user: 'postgres'	
+		//     , password: 'postgres'	    
+		//     ,dumpPath: '/home/shinichi/dir2/test.sql'
+		//     , database: 'CSODB'
+		// }, function (error, output, message) {
+		//     console.log(error)
+		// });
 	
 	};
 
 	AdminController.export = (req, res) => {
-		console.log("ASd")
-		var tool = new PgTools();
-		 tool.dumpDatabase({
-		    host: 'localhost',
-		    port: 5432,
-		    user: 'postgres',
-		    password: 'postgres',
-		    dumpPath: '/home/shinichi/dir2/test.sql',
-		    database: 'CSODB'
-		}).catch(err=>{
-			console.log(err)
-		});	
+		var filename = Date.parse(new Date())+'.sql';
+		var dir = path.join(__dirname,'..','backup/'+filename );
+		var command,ls;
+		
+		if(configuration.database.password == ""){
+			console.log("1st")
+			command = util.format('pg_dumpall -c -h %s -p %d --data-only -U %s --file=%s -l %s --no-password', configuration.database.host, configuration.database.port, configuration.database.username, dir, configuration.database.database);        
+	        console.log(command)
+	        ls = exec(command);
+	        
+		}else{
+			console.log("2st")
+			command = util.format('pg_dumpall -c -h %s -p %d --data-only -U %s --file=%s -l %s --password %s', configuration.database.host, configuration.database.port, configuration.database.username, dir, configuration.database.database, configuration.database.password);        
+	        console.log(command)
+	        ls = exec((command), function(err, out, code) {
+				
+				});
+
+	        ls.stdin.write(configuration.database.password+" \n")  
+		}
+		
+  
 	};
 	
 	return AdminController;
