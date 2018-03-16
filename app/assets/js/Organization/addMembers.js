37bb8c3fbc1b7fe694b79d78b95b510c688c1f7b
Dominique Dagunton <dedagunton@yahoo.com>
2018-03-17 01:51:24 +0800

    working on addmember js

--- app/assets/js/Organization/addMembers.js
+++ app/assets/js/Organization/addMembers.js
@@ -1,4 +1,53 @@
 $("#myTable").DataTable();
+$("#name").focusout(function(event) {
+   val = $(this).val()
+   if(val == null){
+        $('#fullnamesmall').show();
+   }else{
+        $('#fullnamesmall').hide();
+   }
+});
+$("#idnumber").focusout(function(event) {
+   val = $(this).val()
+   if(val == null){
+        $('#idnumber').show();
+   }else{
+        $('#idnumber').hide();
+   }
+});
+$("#addtolist").click(function(event) {
+    var idnum = $("#idnumber").val()
+    var name = $("#name").val()
+    //validation
+    if(idnum==null || name ==null){
+        $('#addtolist').attr('disabled', 'disabled');
+        if(idnum==null){
+            $('#idnumbersmall').show();
+        }
+        if(name==null){
+            
+        }
+
+    }else{
+        $('#idnumbersmall').hide();
+        $('#fullnamesmall').hide();
+        var add = "<tr>"+
+                    "<td>"+name+"</td>"+
+                    "<td>"+idnum+"</td>"+
+                    '<td><i class="fa fa-pencil m-r-10" data-toggle="tooltip" data-original-title="edit"></i><i class="fa fa-times text-danger m-r-10" data-toggle="tooltip" data-original-title="delete"></i></td>'+
+                +'</tr>'
+        $('tbody').append(add);
+    }
+    
+});
+$(".fa-times").click(function(event) {
+    console.log('fatimes')
+    $('tbody').remove().closest('tr');
+});
+$(".fa-pencil").click(function(event) {
+    console.log('fapencil')
+});
+
 $(document).on('click', '#approve', function() {
 
     swal({
