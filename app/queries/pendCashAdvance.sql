f74bcef5ab53571ad3034f86c2b87748d3062388
neilmd <neil_capistrano2007@yahoo.com>
2018-03-16 22:40:55 +0800

    LaST FIRX

--- app/queries/pendCashAdvance.sql
+++ app/queries/pendCashAdvance.sql
@@ -1,4 +1,4 @@
 UPDATE public."PreActivityCashAdvanceSignatory"
-   SET  status=2,sectionsToEdit=${sections}, comments=${explain}  
+   SET  status=2,"sectionsToEdit"=${sections}, comments=${explain}  
  WHERE signatory=${signatory}
    AND "cashAdvance"=${cashAdvance};
--- app/queries/pendDirectPayment.sql
+++ app/queries/pendDirectPayment.sql
@@ -1,4 +1,4 @@
 UPDATE public."PreActivityDirectPaymentSignatory"
-   SET  status=2, sectionsToEdit=${sections}, comments=${explain} 
+   SET  status=2, "sectionsToEdit"=${sections}, comments=${explain} 
  WHERE signatory=${signatory}
    AND "directPayment"=${directPayment};
