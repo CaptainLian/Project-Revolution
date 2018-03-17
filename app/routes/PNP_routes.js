9fce1c8eb8b11b3730920cb537c2fbe4bb42aa38
Dominique Dagunton <dedagunton@yahoo.com>
2018-03-17 20:54:00 +0800

    added PNP Main again

--- app/routes/PNP_routes.js
+++ app/routes/PNP_routes.js
@@ -3,7 +3,8 @@ module.exports = function(configuration, modules, router, controllers){
 
 	router.get('/PNP/ViewPubs', controllers.PNP_controller.viewPubs);
 	router.get('/PNP/Pubs/List', controllers.PNP_controller.viewPubsList);
-
+	router.get('/PNP/main', controllers.PNP_controller.viewHome);
+ 
 	//PUBS PER ACTIVITY
 	router.get('/PNP/Pubs/:gosmid', controllers.PNP_controller.viewSpecificPubs);
 };
