7df74cf8b9796a161d6d20776bb4f0cbc15f5d89
Dominique Dagunton <dedagunton@yahoo.com>
2018-03-30 02:50:12 +0800

    changed timestamp color to gray

--- app/assets/js/system/notification.js
+++ app/assets/js/system/notification.js
@@ -72,7 +72,7 @@ $('#slimtest1').slimScroll({
           '<a href="#">'+
                 '<span class="profile-status online pull-right"></span>'+
                 '<div class="mail-content breaks">'+
-                '<h5>'+title+'</h5> <span class="mail-desc"><small>'+description+'</small></span><br><span class="time"><small>'+timestamp+'</small></span> </div>'+
+                '<h5>'+title+'</h5> <span class="mail-desc"><small>'+description+'</small></span><br><span class="time text-gray"><small>'+timestamp+'</small></span> </div>'+
             '</a>';
                                     
         $("#display_notifications").append(test)
