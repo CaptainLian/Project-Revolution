65cade7078dae879670903a974f0203acf28b37d
neilmd <neil_capistrano2007@yahoo.com>
2018-04-03 18:07:24 +0800

    Added get_current_tern_id() for PNP list and ADM list

--- app/queries/getPostActsToCheck.sql
+++ app/queries/getPostActsToCheck.sql
@@ -3,5 +3,7 @@ SELECT TO_CHAR(PP.actualDateStart,'Mon DD, YYYY') as ddate, PPP."status" as psta
   							       ON PPP."GOSMActivity" = PP.GOSMActivity
   							     JOIN GOSMACTIVITY GA
   							       ON PP.GOSMACTIVITY = GA.ID
- WHERE PPP."status" = 3 OR PPP."status" = 4 OR PPP."status" = 5
- ;
+  							     JOIN GOSM G
+  							       ON GA.GOSM = G.ID
+ WHERE (PPP."status" = 3 OR PPP."status" = 4 OR PPP."status" = 5)
+   AND G.TERMID = system_get_current_term_id();
